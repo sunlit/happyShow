6463589cb6d9351b870f0264ee3bd9c1e9f35cc6
Sunlit <sunlit.jiang@foxmail.com>
2012-06-02 07:47:02 +0800

    Drawable animation loads a series of resources one after another to create an animation. It's important to note that the start() method called on the AnimationDrawable cannot be called during the onCreate() method of Activity, because the AnimationDrawable is not yet fully attached to the window. To play the animation immediately, call start from the onWindowFocusChanged() method in Activity, which will get called when Android brings your window into focus.

--- src/com/sunlitjiang/happyShow/HappyShowActivity.java
+++ src/com/sunlitjiang/happyShow/HappyShowActivity.java
@@ -1,13 +1,32 @@
 package com.sunlitjiang.happyShow;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class HappyShowActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	private ImageView showedImage;
+	private AnimationDrawable frameAnimation;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Load the ImageView that will host the animation and
+        // set its background to our AnimationDrawable XML resource.
+        showedImage = (ImageView) findViewById(R.id.imageView_showedPic);
+        showedImage.setBackgroundResource(R.drawable.slides);
+        // Get the background, which has been compiled to an AnimationDrawable object.
+        frameAnimation = (AnimationDrawable) showedImage.getBackground();
     }
+	
+	@Override
+	public void onWindowFocusChanged (boolean hasFocus){
+		super.onWindowFocusChanged (hasFocus);
+		frameAnimation.start();
+	}
 }
+
