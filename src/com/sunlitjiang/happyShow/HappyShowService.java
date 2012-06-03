9a0fb7a4c1a8ba07605fa6e73033d4a2010af3a6
Sunlit <sunlit.jiang@foxmail.com>
2012-06-03 22:53:47 +0800

    Create a new branch for test the basic function (the master branch does not work after add a checking for power plug status).

--- src/com/sunlitjiang/happyShow/HappyShowService.java
+++ src/com/sunlitjiang/happyShow/HappyShowService.java
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -30,13 +29,6 @@ public class HappyShowService extends Service {
 
 		screenoffReciever = new BroadcastReceiver(){  
 			public void onReceive(Context context, Intent intent) {
-				IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-				Intent batteryStatus = context.registerReceiver(null, intentFilter);
-
-				int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-				boolean bPlugged =  (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) ||
-									(chargePlug == BatteryManager.BATTERY_PLUGGED_AC);
-				if (bPlugged) {
 					try {  
 						Intent intentToActivity = new Intent();  
 						intentToActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
@@ -45,7 +37,6 @@ public class HappyShowService extends Service {
 					} catch (Exception e) {  
 						Log.i("Exception in HappyShowService:", e.toString());  
 					}  
-				}
 			} //onReceive
 		};
 		registerReceiver(screenoffReciever, new IntentFilter(Intent.ACTION_SCREEN_OFF));
